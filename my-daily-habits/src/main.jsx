8685bded7e9ef854de9af5ec3e12b2fd5524d014
Alefe <alefealvesdacosta@gmail.com>
2026-03-27 18:26:11 -0400

    Testando nova configuração para o deploy com o HashRouter

--- my-daily-habits/src/main.jsx
+++ my-daily-habits/src/main.jsx
@@ -1,16 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+/*import { BrowserRouter } from 'react-router-dom';*/
+import { HashRouter } from 'react-router-dom';
 import { HabitsProvider } from './contexts/HabitsContext.jsx';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/primeiro-projeto-react/my-daily-habits'>
+    <HashRouter >
       <HabitsProvider>
         <App/>
       </HabitsProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
