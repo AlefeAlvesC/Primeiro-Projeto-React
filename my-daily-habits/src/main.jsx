81c8f7265f28a0d705c9374c7b4be9ec04ebe56f
Alefe <alefealvesdacosta@gmail.com>
2026-03-27 18:11:57 -0400

    Adicionando configurações de deploy para usar o github pages com react

--- my-daily-habits/src/main.jsx
+++ my-daily-habits/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/primeiro-projeto-react/my-daily-habits'>
       <HabitsProvider>
         <App/>
       </HabitsProvider>
